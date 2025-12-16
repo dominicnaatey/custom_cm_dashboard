d002fd5d095bc73ee9b471c9b83e3a0b31438a15
dominicnaatey <dominicnaatey@gmail.com>
2025-12-16 23:55:27 +0000

    lazy load VisitorChart component with dynamic import

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -1,11 +1,17 @@
 'use client';
 
 import React from 'react';
+import dynamic from 'next/dynamic';
 import { TopCards } from './TopCards';
-import { VisitorChart } from './VisitorChart';
+// import { VisitorChart } from './VisitorChart';
 import { RecentContent } from './RecentContent';
 import { QuickStats } from './QuickStats';
 import { ActivityFeed } from './ActivityFeed';
+
+const VisitorChart = dynamic(() => import('./VisitorChart').then(mod => mod.VisitorChart), { 
+  ssr: false,
+  loading: () => <div className="h-[300px] w-full bg-gray-100 dark:bg-gray-800 rounded-2xl animate-pulse"></div>
+});
 
 export const Dashboard: React.FC = () => {
   return (
